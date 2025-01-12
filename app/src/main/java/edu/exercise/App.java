bc7d0cad69825ab91f3d53e8f7389d3577f15427
crockdaly <baninmatvey333@gmail.com>
2025-01-13 08:43:25 +1000

    test

---
+++ app/src/main/java/edu/exercise/App.java
@@ -0,0 +1,9 @@
+package edu.exercise;
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ app/src/test/java/edu/exercise/AppTest.java
@@ -0,0 +1,9 @@
+
+package edu.exercise;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+}
