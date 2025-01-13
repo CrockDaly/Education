78a520059970788349380cbff5ec67594ad7fc4e
crockdaly <baninmatvey333@gmail.com>
2025-01-13 13:38:14 +1000

    Java Polymorphism

---
+++ app/src/main/java/edu/exercise/Polymorphism/exercise2/Motorcycle.java
@@ -0,0 +1,10 @@
+package edu.exercise.Polymorphism.exercise2;
+
+public class Motorcycle extends Vehicle {
+
+    @Override
+    public void speedUp() {
+        super.speedUp();
+        System.out.println("Motorcycle speeds up by 15 units");
+    }
+}
---
+++ app/src/main/java/edu/exercise/Polymorphism/exercise2/Vehicle.java
@@ -0,0 +1,13 @@
+package edu.exercise.Polymorphism.exercise2;
+
+public class Vehicle {
+    private int speed;
+
+    public void speedUp() {
+        speed += 10;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
