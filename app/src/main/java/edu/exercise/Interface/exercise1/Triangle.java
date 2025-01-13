17f44c2da9470f492391ac00b0dcd07f1d9156ec
crockdaly <baninmatvey333@gmail.com>
2025-01-13 13:57:49 +1000

    Java Interface

---
+++ app/src/main/java/edu/exercise/Interface/exercise1/Triangle.java
@@ -0,0 +1,16 @@
+package edu.exercise.Interface.exercise1;
+
+public class Triangle implements Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return base*height * 0.5;
+    }
+}
