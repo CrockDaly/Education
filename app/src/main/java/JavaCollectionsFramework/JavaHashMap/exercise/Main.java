bea514b1833778da79232bf03aea8d88255353f0
crockdaly <baninmatvey333@gmail.com>
2025-01-23 21:12:25 +1000

    JavaHashMap 1, 2, 3, 4 и 7 задания

---
+++ app/src/main/java/JavaCollectionsFramework/JavaHashMap/exercise/Main.java
@@ -0,0 +1,15 @@
+package JavaCollectionsFramework.JavaHashMap.exercise;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Vasya", 14);
+        map.put("Oleg", 16);
+        map.put("Katya", 19);
+        map.put("Igor", 20);
+
+        System.out.println(map);
+    }
+}
