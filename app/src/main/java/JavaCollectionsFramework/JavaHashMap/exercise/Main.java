262ae1c6bbbdac095de2602e17d2f406b02055e6
Matvey <74357595+CrockDaly@users.noreply.github.com>
2025-01-24 22:34:08 +1000

    Update Main.java
    Добавил полиморфизм

--- app/src/main/java/JavaCollectionsFramework/JavaHashMap/exercise/Main.java
+++ app/src/main/java/JavaCollectionsFramework/JavaHashMap/exercise/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         map.put("Vasya", 14);
         map.put("Oleg", 16);
         map.put("Katya", 19);
