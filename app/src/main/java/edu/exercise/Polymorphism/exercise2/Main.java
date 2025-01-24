106f3c54197656aa9262337122320b3c4923484e
Matvey <74357595+CrockDaly@users.noreply.github.com>
2025-01-24 22:31:46 +1000

    Update Main.java
    добавил полиморфизм

--- app/src/main/java/edu/exercise/Polymorphism/exercise2/Main.java
+++ app/src/main/java/edu/exercise/Polymorphism/exercise2/Main.java
@@ -3,7 +3,7 @@ package edu.exercise.Polymorphism.exercise2;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
-        Motorcycle motorcycle = new Motorcycle();
+        Car motorcycle = new Motorcycle();
 
         System.out.println("Car inital speed: " + car.getSpeed());
         System.out.println("Motorcycle inital speed: " + motorcycle.getSpeed());
