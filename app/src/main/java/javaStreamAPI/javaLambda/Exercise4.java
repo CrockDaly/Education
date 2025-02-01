b3fb81128129c02f7bc71d88b5ddfd614ee84b94
crockdaly <baninmatvey333@gmail.com>
2025-02-02 05:02:19 +1000

    JavaLambda

---
+++ app/src/main/java/javaStreamAPI/javaLambda/Exercise4.java
@@ -0,0 +1,30 @@
+package javaStreamAPI.javaLambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Write a Java program to implement a lambda expression to filter out even and odd numbers from a list of integers.
+public class Exercise4 {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+
+        System.out.println("The original list is: " + arr);
+
+        List<Integer> oddNumbers = arr.stream()
+                .filter(element -> element % 2 == 0 )
+                .collect(Collectors.toList());
+        System.out.println("Odd numbers of the list is: " +oddNumbers);
+
+        List<Integer> evenNumbers = arr.stream()
+                .filter(element -> element % 2 != 0)
+                .collect(Collectors.toList());
+        System.out.println("Even numbers of the list is: " +evenNumbers);
+
+    }
+}
---
+++ app/src/main/java/javaStreamAPI/javaLambda/Exercise5.java
@@ -0,0 +1,24 @@
+package javaStreamAPI.javaLambda;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Write a Java program to implement a lambda expression to sort a list of strings in alphabetical order.
+public class Exercise5 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Kolya");
+        list.add("Vasya");
+        list.add("Mary");
+        list.add("Alex");
+        list.add("Bob");
+
+        System.out.println("The original list is: " + list);
+
+        list.sort(String::compareTo);
+        System.out.println("The sorted list is: " + list);
+    }
+}
