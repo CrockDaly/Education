cbea2d38cbf209bf52d715c5e8f0f1dc3a33db42
Matvey <74357595+CrockDaly@users.noreply.github.com>
2025-02-16 12:49:26 +1000

    Update CalculatorTest.java

--- app/src/test/java/javatests/exercise1/CalculatorTest.java
+++ app/src/test/java/javatests/exercise1/CalculatorTest.java
@@ -2,14 +2,18 @@ package javatests.exercise1;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //1. Write a Java unit test case to assert that a given method returns the expected value.
 public class CalculatorTest {
-    @Test
-    void testAdd() {
+    @ParameterizedTest
+    @CsvSource({
+        "3, 5, 8",      // 3 + 5 = 8
+        "10, 0, 10",    // 10 + 0 = 10
+        "-10, 8, -2"    // -10 + 8 = -2
+    })
+    void testAdd(int a, int b, int expected) {
         Calculator calculator = new Calculator();
-
-        int result = calculator.add(3, 5);
-        assertEquals(8, result);
+        assertEquals(expected, calculator.add(a, b));
     }
 }
