2e9affc87103d39fc709211b7db2b75905d074d3
Matvey <74357595+CrockDaly@users.noreply.github.com>
2025-02-16 12:37:19 +1000

    Update ExampleClassTest.java

--- app/src/test/java/javatests/exercise2/ExampleClassTest.java
+++ app/src/test/java/javatests/exercise2/ExampleClassTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 //2. Write a Java test case to verify that a specific exception is thrown when a method is called with invalid input.
 public class ExampleClassTest {
     @Test
-    void testDivideByZero() {
+    void testDivideByZeroThrowsException() {
         assertThrows(IllegalArgumentException.class, () -> ExampleClass.divide(1, 0));
     }
 }
